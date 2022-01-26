f8b20224b27294b68c4149d91b4880d8a0331ebf
denisprohorov <denprohorov8@yandex.com>
2022-01-26 18:13:22 +0300

    Merge our code

--- modules/map/hex.cpp
+++ modules/map/hex.cpp
@@ -36,11 +36,10 @@ void Hex::Free() {
 }
 
 int Hex::GetDistance(Hex &f, Hex &s) {
-//    std::tuple<int, int, int> [x1, y1, z1] = f.coordinates;
-//    auto& [x2, y2, z2] = s.coordinates;
-//    get<0>(f.coordinates)
-//    return (abs(x1 - x2) + abs(y1 - y2) + abs(z1 - z2)) / 2;
-    return 0;
+    auto&[x1, y1, z1] = f.coordinates;
+    auto&[x2, y2, z2] = s.coordinates;
+    return (abs(x1 - x2) + abs(y1 - y2) + abs(z1 - z2)) / 2;
+//    return 0;
 }
 
 void Hex::SetOwnerId(int id) {
