e0daf7b0c0baf8bc98db5007f390ffcf0d376221
denisprohorov <denprohorov8@yandex.com>
2022-02-12 11:59:16 +0300

    install sfml

--- README.md
+++ README.md
@@ -1,15 +1,13 @@
 # Pigs-on-the-moon
 
-## Stage 1
-
-If the application is built main starts. In main we can run the game with a different number of players, and bot
-finishes the game. Rival bots in our case are standing players needed to start the game.
-
 ## Stage 2
+
 Added support for new tanks. The bot can walk and kill enemy tanks. Required libraries are connected via conan.
 
+## Conan dependency
 
-
+nlohmann_json/3.10.5 - static lib\
+sfml/2.5.1 - shared lib
 
 ## Instruction
 
@@ -24,3 +22,11 @@ mkdir conanBuild
 cd conanBuild
 conan install ..
 ```
+As sfml is a shared lib in folder conanBuild creates bin folder. Move this folder to bin folder
+with executable file.
+
+## Some words about development
+
+Lib sfml has two versions: debug and release. If you want run project in debug mode 
+redact conan profile. In particular I set build_type=Debug in user/.conan/profile/default.\
+Or you can use conan install .. -s build_type=Debug.
--- main.cpp
+++ main.cpp
@@ -1,42 +1,21 @@
 #include "gameClient.h"
 #include <iostream>
-#include <thread>
+#include <SFML/Graphics.hpp>
 
-using namespace std;
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML Works!");
 
-//thread function. One thread - one bot.
-void ClientThreadFunction(const string &name, const string &password, const string &gameName, int numTurns, int numPlayers) {
-    GameClient gc;
-    gc.StartAI(name, password, gameName, numTurns, numPlayers);
-}
-
-
-int main() {
-    ///////////////////////////////////////////////////////////////////////////////variables for debugging
-    std::string gameName = "Pigs-on-the-moon";// the name of the game we are connecting to
-    const int playersCount = 3;               // number of players. Values from [1, 2, 3]
-    const int numbersTurn = 100;              // numbers of turns. Values from [0 ... 100]
-    int ourOrder = 1;                         // our connection number. Values from [1, 2, 3] // may be more than numberCount
-    ///////////////////////////////////////////////////////////////////////////////
-
-    ourOrder = min(ourOrder, playersCount);// simple check for easy life
-
-    gameName += "_" + std::to_string(playersCount) + "_" + std::to_string(ourOrder);
-
-    std::vector<std::thread> threads;
-
-    int botCount = 0;
-    for (int i = 0; i < playersCount; ++i) {// Login all
-        if (i == ourOrder - 1) {
-            threads.emplace_back(ClientThreadFunction, "overmind", "", gameName, numbersTurn, playersCount); // our overmind
-            continue;
-        }
-        threads.emplace_back(ClientThreadFunction, "bot" + std::to_string(botCount), "", gameName, numbersTurn, playersCount);
-        ++botCount;
-    }
-
-    for (int i = 0; i < threads.size(); ++i) {// wait all our children's to avoid run time exception
-        threads[i].join();
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+       {
+            if (event.type == sf::Event::Closed)
+                window.close();
+       }
+       window.clear(sf::Color(250,220,100,0));
+        window.display();
     }
 
     return 0;
