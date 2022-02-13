b667182e73f845163ac1b868f9c97a236b94823d
denisprohorov <denprohorov8@yandex.com>
2022-02-13 17:17:46 +0300

    add crooked background

--- main.cpp
+++ main.cpp
@@ -1,48 +1,52 @@
 #include "gameClient.h"
 #include "gui/screen.h"
-#include <iostream>
-#include <SFML/Graphics.hpp>
 #include <SFML/Audio.hpp>
+#include <SFML/Graphics.hpp>
+#include <iostream>
 
 double xPosition = 0.2;
 double yPosition = 0.2;
-double width = 0.4;
-double height = 0.4;
+double width = 0.5;
+double height = 0.6;
 
-int main()
-{
-    Screen screen (sf::VideoMode::getDesktopMode().width, sf::VideoMode::getDesktopMode().height);
+int main() {
+    Screen screen(sf::VideoMode::getDesktopMode().width, sf::VideoMode::getDesktopMode().height);
     sf::RenderWindow window(sf::VideoMode(screen.width * width, screen.height * height), "WoT_strategy_Pigs-on-the-moon");
     window.setPosition(sf::Vector2i(screen.width * xPosition, screen.height * yPosition));
     window.setFramerateLimit(30);
 
     sf::Music music;
     if (!music.openFromFile("resources/music/Intro.ogg"))
-        return -1; // error
+        std::cerr << "can't load Intro music" << '\n';
     music.play();
-//    GameClient gc;
-//    gc.InitGame("Den-Pig");
-//    gc.InitIds();
-//    gc.
+
+        GameClient gc;
+        gc.InitGame("Den-Pig");
+        gc.InitIds();
+
+    sf::Texture texture;
+    if (!texture.loadFromFile("resources/image/background.jpg")) {
+        std::cerr << "can't load texture" << '\n';
+    }
+    sf::Sprite sprite;
+    sprite.setTexture(texture);
 
 
-
-    while (window.isOpen())
-    {
+    while (window.isOpen()) {
         sf::Event event;
-        while (window.pollEvent(event))
-       {
-            if (event.type == sf::Event::Closed)
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed) {
                 window.close();
-       }
+            }
+        }
 
-       sf::CircleShape hex(50.f, 6);
-       hex.setFillColor(sf::Color(100, 250, 50));
-       hex.rotate(30.f);
-       window.draw(hex);
+        sf::CircleShape hex(50.f, 6);
+        hex.setFillColor(sf::Color(100, 250, 50));
+        hex.setRotation(30.f);
+        window.draw(sprite);
+        window.draw(hex);
 
-//       window.clear(sf::Color(250,220,100,0));
-       window.display();
+        window.display();
     }
 
     return 0;
--- modules/gui/screen.h
+++ modules/gui/screen.h
@@ -1,7 +1,9 @@
 #pragma once
+#include <SFML/Graphics.hpp>
 
 struct Screen{
     unsigned int width;
     unsigned int height;
     Screen(unsigned int width, unsigned int height) : width(width), height(height) {}
+
 };
